899a70ff3115c038bec81310e7a8f42ff239415d
joseluiz2323 <lui_zzzz@hotmail.com>
2022-04-22 21:13:54 -0300

    main

---
+++ qt5/login.py
@@ -0,0 +1,21 @@
+#remover a primeira linha de um arquivo
+
+
+remover_linha('extencaobot\\logs\\log_de_criacao.txt', "1-08:53:40-Criado\n")
+self.listViewAba1.setModel(
+    QtCore.QStringListModel(lista2))
+self.listViewAba1.setStyleSheet(
+    "QListView::item {"
+    "  margin-top: 2px;"
+    "  margin-bottom: 2px;"
+    "  margin-left: 10px;"
+    "  margin-right: 10px;"
+    "  border-width: 1px;"
+    "  border-color: #999;"
+    "  border-radius: 9px;"
+    "  background: #FFFF64;"
+    "  padding: 10px;"
+    "}"
+    "QListView::item:selected:active {"
+    "  background: #aaaaaa;"
+    "}")
